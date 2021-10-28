92365f72c5cd242864b4348575a496518bd0865a
Bram Knook <454395@student.fontys.nl>
2021-10-28 22:33:49 +0200

    Files toevoegen

---
+++ src/main/java/com/example/loonaverse/LoonaverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.loonaverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoonaverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoonaverseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loonaverse/LoonaverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loonaverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoonaverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
