e7de13001f8f35e3e96c96173c65e8fae0422f38
Bram Knook <454395@student.fontys.nl>
2022-06-07 21:32:24 +0200

    integration test + ci

--- src/test/java/com/example/loonaverse/web/SongResourceIT.java
+++ src/test/java/com/example/loonaverse/web/SongResourceIT.java
@@ -1,10 +1,53 @@
 package com.example.loonaverse.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SongResourceIT {
-        
+@ExtendWith(SpringExtension.class)
+class SongResourceIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Container
+    private static MySQLContainer<?> mySQLdb = new MySQLContainer<>("mysql:latest")
+            .withDatabaseName("Loonaverse")
+            .withUsername("Loonaverse")
+            .withPassword("Loonaverse")
+            .withExposedPorts(3306);
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () ->
+                String.format("jdbc:mysql://%s:%s/Loonaverse", mySQLdb.getHost(), mySQLdb.getFirstMappedPort()));
+    }
+
+    @Test
+    void getAllSongs() throws Exception {
+        mockMvc.perform(
+                get("/songs")
+        ).andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(12));
+    }
 }
